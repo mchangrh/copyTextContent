f1b3190d3ed4cbff64a15ca9f81809bf33532fe2
Michael C <michael@mchang.name>
2023-01-28 22:14:14 -0500

    undo optimizations in chrome-lite and firefox

--- chrome-lite/src/background.js
+++ chrome-lite/src/background.js
@@ -5,19 +5,18 @@ chrome.contextMenus.create({
   contexts: ["selection"] // only selection since we can only get selection without using content scripts
 })
 
-function copyTarget(target) {
-  if (!target) return // return if no target
-  const text = target?.textContent // get textContent
-  if (!text) return // return if no text to copy
-  navigator.clipboard.writeText(text) // copy text
-}
-
 function onClickedListener(info, tab) {
   if (!info.menuItemId === "copy-text-content") return // check if correct menu item
   chrome.scripting.executeScript({
     injectImmediately: true,
     target: { tabId: tab.id }, // target the current tab
-    func: () => copyTarget(document.getSelection()?.anchorNode)
+    func: () => {
+      const target = document.getSelection()?.anchorNode
+      if (!target) return // return if no target
+      const text = target?.textContent // get textContent
+      if (!text) return // return if no text to copy
+      navigator.clipboard.writeText(text) // copy text
+    }
   })
 }
 
--- firefox/src/background.js
+++ firefox/src/background.js
@@ -5,19 +5,18 @@ browser.menus.create({
   contexts: ["editable", "link", "page", "selection"]
 })
 
-function copyTarget(target) {
-  if (!target) return // return if no target
-  const text = target?.textContent // get textContent
-  if (!text) return // return if no text to copy
-  navigator.clipboard.writeText(text) // copy text
-}
-
 browser.menus.onClicked.addListener((info, tab) => {
   if (!info.menuItemId === "copy-text-content") return
   browser.scripting.executeScript({
     injectImmediately: true,
     target: { tabId: tab.id }, // target the current tab
     args: [info.targetElementId],
-    func: (args) => copyTarget(browser.menus.getTargetElement(args))
+    func: (args) => {
+      const target = browser.menus.getTargetElement(args)
+      if (!target) return // return if no target
+      const text = target?.textContent // get textContent
+      if (!text) return // return if no text to copy
+      navigator.clipboard.writeText(text) // copy text
+    }
   })
 })
